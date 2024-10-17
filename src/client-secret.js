6017669dc653fc89b86e461e835271801f6dddd5
Danilo Spinelli <danilo.spinelli@pagopa.it>
2024-10-17 15:36:20 +0200

    Cut secret to 4 chars

--- src/client-secret.js
+++ src/client-secret.js
@@ -6,7 +6,7 @@ function getClientSecret(clientId, secret) {
   const hash = crypto.createHash("sha256").update(combined).digest("hex");
   const integerHash = BigInt("0x" + hash);
   const base36 = integerHash.toString(36).toUpperCase();
-  return base36.substring(0, 8);
+  return base36.substring(0, 4);
 }
 
 module.exports = getClientSecret;
